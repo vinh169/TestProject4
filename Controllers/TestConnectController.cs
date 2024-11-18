36b065714bc659f8393109116395ba9afddb8420
vinh169 <ngothevinh169@gmail.com>
2024-11-18 14:25:37 +0700

    test

---
+++ Controllers/TestConnectController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestProject.Repository;
+
+namespace TestProject.Controllers
+{
+    [ApiController]
+    [Route("/api/testConnect")]
+    public class TestConnectController : ControllerBase
+    {
+        public TestConnectionRepository connectionRepository;
+
+        public TestConnectController(TestConnectionRepository connectionRepository)
+        {
+            this.connectionRepository = connectionRepository;
+        }
+
+        [HttpGet]
+        public ActionResult testConnect()
+        {
+            return Ok(connectionRepository.TestConnection());
+        }
+    }
+}
