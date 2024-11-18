fcc67a7bffb69bb151c19799bb452dfb31cd4de6
vinh169 <ngothevinh169@gmail.com>
2024-11-18 14:33:23 +0700

    git init

---
+++ Model/User.cs
@@ -0,0 +1,23 @@
+﻿namespace TestProject.Model
+{
+    public class User
+    {
+        public int User_Id { get; set; }
+        public string User_Name { get; set; }
+        public string User_Passkey { get; set; }
+        public string User_Token { get; set; }
+        public int Status { get; set; }
+        public DateTime Date_Of_Birth { get; set; }
+        public string Full_name { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string Mobile { get; set; }
+        public DateTime Created_Date { get; set; }
+        public DateTime Update_Date { get; set; }
+        public string Created_User { get; set; }
+        public string Update_User { get; set; }
+        public int Profession_Id { get; set; }
+        public int Collaborator_Id { get; set; }
+        public int Active {get; set; }
+    }
+}
---
+++ Repository/TestConnectionRepository.cs
@@ -0,0 +1,36 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System.Data;
+
+namespace TestProject.Repository
+{
+    public class TestConnectionRepository
+    {
+        IConfiguration configuration;
+
+        public TestConnectionRepository(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public string TestConnection()
+        {
+            using (var conn = this.GetConnection())
+            {
+                if (conn == null)
+                {
+                    return "Connect failed";
+                }
+                return "Connect success";
+            }
+        }
+
+        public IDbConnection GetConnection()
+        {
+            //Đọc chuỗi kết nối từ appsettings.json qua IConfiguration.
+            var connectionString = configuration.GetSection("OracleConnection")
+                .GetSection("ConnectionString").Value;
+            var conn = new OracleConnection(connectionString);
+            return conn;
+        }
+    }
+}
